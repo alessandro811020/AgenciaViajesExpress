34476e57d9024390184a04702031d1d1baa0ff97
Alessandro811020 <alejandro-diaz@live.com>
2018-07-24 02:27:35 +0200

    Se ha terminado el footer

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.set('views', path.join(__dirname,'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('hbs', hbs({defaultLayout:'main'}));
-
 app.set('view engine', 'hbs');
 
 app.set('port', (process.env.PORT||PORT));
@@ -22,7 +21,24 @@ app.get('/',(req,res)=>{
     });
 })
 
+
 app.listen(app.get('port'),()=>{
     console.log(`Inicializado el servidor de http://localhost:${app.get('port')}`);
     
 })
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
