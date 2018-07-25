d8470f6408d14da329f23e1b1bbbeccc2c5d6bda
Alessandro811020 <alejandro-diaz@live.com>
2018-07-25 03:59:19 +0200

    mostradas las ofertas desde la base de datos

--- models/Travel.js
+++ models/Travel.js
@@ -6,6 +6,9 @@ const Oferta = CONN.define('ofertas',{
     ciudad: Sequelize.STRING,
     precio: Sequelize.INTEGER,
     reserva: Sequelize.INTEGER,
+    paraid: Sequelize.STRING
 });
+
+
 
 module.exports = Oferta;
